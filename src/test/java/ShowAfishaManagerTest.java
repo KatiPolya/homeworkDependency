fa37edd7f7703883ac7deaf4f7b78513903505c5
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-03-29 16:28:44 +0200

    Changes

--- src/test/java/ShowAfishaManagerTest.java
+++ src/test/java/ShowAfishaManagerTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShowAfishaManagerTest {
-    AfishaManager manager = new AfishaManager(4);
+    AfishaManager manager = new AfishaManager(5);
 
     @Test
     void show() {
